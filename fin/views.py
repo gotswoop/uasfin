0bd5bfaef386d7072faa0f5c803cf464c2ee7ff8
Swaroop Samek <swaroop.samek@gmail.com>
2019-02-01 16:02:52 -0800

    clean up

--- fin/views.py
+++ fin/views.py
@@ -18,8 +18,7 @@ from django.conf import settings
 import logging
 
 access_token = None
-client = plaid.Client(client_id=settings.PLAID_CLIENT_ID, secret=settings.PLAID_SECRET,
-                      public_key=settings.PLAID_PUBLIC_KEY, environment=settings.PLAID_ENV, api_version='2018-05-22')
+client = plaid.Client(client_id=settings.PLAID_CLIENT_ID, secret=settings.PLAID_SECRET, public_key=settings.PLAID_PUBLIC_KEY, environment=settings.PLAID_ENV, api_version='2018-05-22')
 logger = logging.getLogger(__name__)
 
 # ------------------------------------------------------------------
