325b59884f05274e9f60dd0ccdd3abb480b8904d
Swaroop Samek <swaroop.samek@gmail.com>
2019-04-12 21:53:29 -0700

    New signal on post_save of user to trigger user password reset link generation.

--- uasfin2/urls.py
+++ uasfin2/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+# from users.decorators import log_password_reset
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +28,7 @@ urlpatterns = [
     # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    # path('password-reset-complete/', log_password_reset(auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html')), name='password_reset_complete'),
     path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('password-reset/', user_views.passwordReset, name='password_reset'),
     path('ping/', user_views.ping, name='ping'),
--- users/signals.py
+++ users/signals.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from django.conf import settings
@@ -6,55 +8,51 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 
-from .models import Password_Reset_Links
+from users.models import Password_Reset_Links
 from fin.models import User_Actions
 from django.contrib.auth.models import User
 
-
 @receiver(user_logged_in)
 def sig_user_logged_in(sender, user, request, **kwargs):
-    # logger = logging.getLogger(__name__)
-    # logger.info("user logged in: %s at %s" % (user, request.META['REMOTE_ADDR']))
-    # Updating the user_password_reset_link table 
-    # url = ('https://' if request.is_secure() else 'http://') + request.get_host() + '/password-reset-confirm/'
-    url = '/password-reset-confirm/'
-    token = url + urlsafe_base64_encode(force_bytes(user.pk)) + '/' + default_token_generator.make_token(user) + '/'
-    updated_item, new_item = Password_Reset_Links.objects.update_or_create(
-        user_id = user, username = user.username, defaults={"reset_token": token}
-    )
-    # TODO try this..
-    new_item = User_Actions.objects.create(
-        user_id = user, 
-        user_ip = request.META['REMOTE_ADDR'],
-        action = "login",
-    )
+	# Updating the user_password_reset_link table 
+	# url = ('https://' if request.is_secure() else 'http://') + request.get_host() + '/password-reset-confirm/'
+	url = '/password-reset-confirm/'
+	token = url + urlsafe_base64_encode(force_bytes(user.pk)) + '/' + default_token_generator.make_token(user) + '/'
+	updated_item, new_item = Password_Reset_Links.objects.update_or_create(
+		user_id = user, username = user.username, defaults={"reset_token": token}
+	)
+	# TODO try this..
+	new_item = User_Actions.objects.create(
+		user_id = user, 
+		user_ip = request.META['REMOTE_ADDR'],
+		action = "login",
+	)
     
 @receiver(user_logged_out)
 def sig_user_logged_out(sender, user, request, **kwargs):
-    if not request.user.is_authenticated:
-        return
+	if not request.user.is_authenticated:
+		return
 
-    # TODO try this..
-    new_item = User_Actions.objects.create(
-        user_id = user, 
-        user_ip = request.META['REMOTE_ADDR'],
-        action = "logout",
-    )
+	# TODO try this..
+	new_item = User_Actions.objects.create(
+		user_id = user, 
+		user_ip = request.META['REMOTE_ADDR'],
+		action = "logout",
+	)
 
-'''
-@receiver(pre_save, sender=User)
+# Creating a new password reset link right after a password reset.
+@receiver(post_save, sender=get_user_model())
 def sig_user_password_reset(sender, **kwargs):
-    user = kwargs.get('instance', None)
-    if user:
-        new_password = user.password
-        try:
-            old_password = User.objects.get(pk=user.pk).password
-        except User.DoesNotExist:
-            old_password = None
-        if new_password != old_password:
-        	new_item = Actions.objects.create(
-     	   		user_id = user, 
-        		user_ip = request.META['REMOTE_ADDR'],
-        		action = "logout",
-    		)
-'''
+	user = kwargs.get('instance', None)
+	if user:
+		url = '/password-reset-confirm/'
+		token = url + urlsafe_base64_encode(force_bytes(user.pk)) + '/' + default_token_generator.make_token(user) + '/'
+		updated_item, new_item = Password_Reset_Links.objects.update_or_create(
+			user_id = user, username = user.username, defaults={"reset_token": token}
+		)
+		# TODO: Move this to a decorator to get remote IP
+		new_item = User_Actions.objects.create(
+			user_id = user, 
+			user_ip = '-',
+			action = "password-change",
+		)
